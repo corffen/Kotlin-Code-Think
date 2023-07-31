aea0b17ae8243609339bfdbc09c384eddba4d36f
corffen <corffen@gmail.com>
2023-07-31 22:48:49 +0800

    反转k个字符串

--- src/main/kotlin/com/example/string/StringSolution.kt
+++ src/main/kotlin/com/example/string/StringSolution.kt
@@ -1,8 +1,35 @@
 package com.example.string
 
+import kotlin.math.min
+
 class StringSolution {
 
     fun reverseString(s: CharArray): Unit {
         s.reverse()
     }
+
+    fun reverseStr(s: String, k: Int): String {
+        val n = s.length
+        val chars = s.toCharArray()
+        var i = 0
+        while (i < n) {
+            reverse(chars, i, min(n, i + k) - 1)
+            i += 2 * k
+        }
+        return String(chars)
+    }
+
+    private fun reverse(arr: CharArray, l: Int, r: Int) {
+        var left = l
+        var right = r
+        while (left < right) {
+            val temp = arr[left]
+            arr[left] = arr[right]
+            arr[right] = temp
+            left++
+            right--
+        }
+    }
+
+
 }
