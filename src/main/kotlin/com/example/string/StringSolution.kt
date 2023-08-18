f3de1adf33abedeafac87d9a97c38495e26a2c40
corffen <corffen@gmail.com>
2023-08-18 21:30:36 +0800

    str索引功能

--- src/main/kotlin/com/example/string/StringSolution.kt
+++ src/main/kotlin/com/example/string/StringSolution.kt
@@ -102,6 +102,10 @@ class StringSolution {
         }
         return sb.toString()
     }
+
+    fun strStr(haystack: String, needle: String): Int {
+        return haystack.indexOf(needle)
+    }
 }
 
 fun main() {
