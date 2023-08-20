5bfc724c88be39477dfdf5f63a7fe70ace68fba7
corffen <corffen@gmail.com>
2023-08-20 18:03:53 +0800

    修改 双指针类的类名

--- src/main/kotlin/com/example/twopoint/TwoPointSolution.kt
+++ src/main/kotlin/com/example/twopoint/TwoPointSolution.kt
@@ -3,39 +3,78 @@ package com.example.twopoint
 import com.example.ListNode
 
 class TwoPointSolution {
-    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
-        var tmpA = headA
-        var tmpB = headB
-        var lenA = 0
-        var lenB = 0
-        while (tmpA != null) {
-            tmpA = tmpA.next
-            lenA++
-        }
-        while (tmpB != null) {
-            tmpB = tmpB.next
-            lenB++
-        }
-        tmpA = headA
-        tmpB = headB
-        val gap = lenA - lenB
-        if (gap < 0) {
-            return getIntersectionNode(headB, headA)
-        }
-        while (gap > 0) {
-            tmpA = tmpA?.next
-        }
-        while (tmpA != null) {
-            if (tmpB == tmpA) {
-                return tmpB
+    /**
+     * 原地移除[nums]中所有值为[value]
+     *
+     */
+    fun removeElement(nums: IntArray, value: Int): Int {
+        var slow = 0
+        nums.forEach { num ->
+            if (num != value) {
+                nums[slow] = num
+                slow++
             }
-            tmpB = tmpB?.next
-            tmpA = tmpA.next
         }
-
-        return tmpB
+        return slow
     }
 
+    fun reverseString(s: CharArray): Unit {
+        s.reverse()
+    }
+
+    fun reverseString2(s: CharArray): Unit {
+        var l = 0
+        var r = s.lastIndex
+        while (l < r) {
+            val tmp = s[l]
+            s[l] = s[r]
+            s[r] = tmp
+            l++
+            r--
+        }
+    }
+
+    fun reverseString3(s: CharArray): Unit {
+        val mid = s.size / 2 - 1
+        if (mid < 0) {
+            return
+        }
+        var r = s.lastIndex
+        for (index in 0..mid) {
+            val tmp = s[index]
+            s[index] = s[r]
+            s[r] = tmp
+            r--
+        }
+    }
+
+    fun reverseList(head: ListNode?): ListNode? {
+        var pre: ListNode? = null
+        var curr = head
+        while (curr != null) {
+            val tmp = curr.next
+            curr.next = pre
+            pre = curr
+            curr = tmp
+        }
+        return pre
+    }
+
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        var fast: ListNode? = head
+        var slow: ListNode? = ListNode(0).apply { next = head }
+        var k = n
+        while (k > 0 && fast != null) {
+            fast = fast.next
+            k--
+        }
+        while (fast != null) {
+            fast = fast.next
+            slow = slow!!.next
+        }
+        slow!!.next = slow.next!!.next
+        return slow
+    }
     fun detectCycle(head: ListNode?): ListNode? {
         var fast = head
         var slow = head
