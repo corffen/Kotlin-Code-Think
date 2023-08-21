e261d2df74b5f2e438e359d9d6c6be0d6e5ca032
corffen-win <corffen@gmail.com>
2023-08-21 20:56:02 +0800

    新增 四数之和

--- src/main/kotlin/com/example/twopoint/TwoPointSolution.kt
+++ src/main/kotlin/com/example/twopoint/TwoPointSolution.kt
@@ -129,4 +129,43 @@ class TwoPointSolution {
         }
         return result
     }
+
+    fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+        val result = ArrayList<List<Int>>()
+        Arrays.sort(nums)
+        for (i in 0 until nums.size) {
+            if (nums[i] > 0&&nums[i]>target) {
+                return result
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue
+            }
+            for (j in i + 1 until nums.size) {
+                if (j > i+1 && nums[j] == nums[j - 1]) {
+                    continue
+                }
+                var left = j + 1
+                var right = nums.size - 1
+                while (left < right) {
+                    val sum = nums[left] + nums[right] + nums[i]+nums[j]
+                    if (sum > target) {
+                        right--
+                    } else if (sum < target) {
+                        left++
+                    } else {
+                        result.add(listOf(nums[i], nums[left], nums[right]))
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--
+                        }
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++
+                        }
+                        left++
+                        right--
+                    }
+                }
+            }
+        }
+        return result
+    }
 }
