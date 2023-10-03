bf984587f758a7c3e134ccf4a72d707a20d1aa7b
corffen-win <corffen@gmail.com>
2023-10-03 16:29:27 +0800

    新增 最大摆动序列

--- src/main/kotlin/com/example/greedy/GreedySolution.kt
+++ src/main/kotlin/com/example/greedy/GreedySolution.kt
@@ -20,3 +20,33 @@ fun findContentChildren(g: IntArray, s: IntArray): Int {
     }
     return count
 }
+
+/**
+ * Wiggle max length
+ *如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为 摆动序列 。第一个差（如果存在的话）可能是正数或负数。仅有一个元素或者含两个不等元素的序列也视作摆动序列。
+ *
+ * 例如， [1, 7, 4, 9, 2, 5] 是一个 摆动序列 ，因为差值 (6, -3, 5, -7, 3) 是正负交替出现的。
+ *
+ * 相反，[1, 4, 7, 2, 5] 和 [1, 7, 4, 5, 5] 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+ * 子序列 可以通过从原始序列中删除一些（也可以不删除）元素来获得，剩下的元素保持其原始顺序。
+ *
+ * 给你一个整数数组 nums ，返回 nums 中作为 摆动序列 的 最长子序列的长度 。
+ * @param nums
+ * @return
+ */
+fun wiggleMaxLength(nums: IntArray): Int {
+    if (nums.size <= 1) {
+        return nums.size
+    }
+    var ans = 1
+    var currDiff = 0
+    var preDiff = 0
+    for (i in 0 until nums.size - 1) {
+        currDiff = nums[i + 1] - nums[i]
+        if (preDiff <= 0 && currDiff > 0 || (preDiff >= 0 && currDiff < 0)) {
+            preDiff = currDiff
+            ans++
+        }
+    }
+    return ans
+}
