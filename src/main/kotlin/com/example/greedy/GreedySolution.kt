0a2b5d3f084185046ae633a1f841d2001370dd99
corffen-win <corffen@gmail.com>
2023-10-04 17:24:21 +0800

    新增 最大子数组和

--- src/main/kotlin/com/example/greedy/GreedySolution.kt
+++ src/main/kotlin/com/example/greedy/GreedySolution.kt
@@ -50,3 +50,22 @@ fun wiggleMaxLength(nums: IntArray): Int {
     }
     return ans
 }
+
+/**
+ * 详情:
+ * [最大子数组和](https://programmercarl.com/0053.%E6%9C%80%E5%A4%A7%E5%AD%90%E5%BA%8F%E5%92%8C.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE)
+ */
+fun maxSubArray(nums: IntArray): Int {
+    var ans = Int.MIN_VALUE
+    var count = 0
+    nums.forEach { num ->
+        count += num
+        if (count > ans) {
+            ans = count
+        }
+        if (count < 0) {
+            count = 0
+        }
+    }
+    return ans
+}
